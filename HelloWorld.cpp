d1f974c7ec57b96229d096241f3f048573f6ad08
Andres Vargas <vargasa@cua.edu>
2020-08-07 18:29:17 -0400

    Add fxn add(): integer addition

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
+int add(const int& a, const int& b){
+  return a+b;
+}
+
 int main(){
 
   std::cout << "Hola Mundo\n";
+  std::cout << "3 + 5 =" << add(3,5) << std::endl;
   return 0;
 
 }
